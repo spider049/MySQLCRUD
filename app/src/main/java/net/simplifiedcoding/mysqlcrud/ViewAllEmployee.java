89533e509d6818c50e29c5daf200aef8d4651739
spideryom <spideryom049@gmail.com>
2017-04-05 12:14:55 +0700

    แก้ไข ข้อความ Fetching Asynctask

--- app/src/main/java/net/simplifiedcoding/mysqlcrud/ViewAllEmployee.java
+++ app/src/main/java/net/simplifiedcoding/mysqlcrud/ViewAllEmployee.java
@@ -3,16 +3,13 @@ package net.simplifiedcoding.mysqlcrud;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -74,7 +71,7 @@ public class ViewAllEmployee extends AppCompatActivity implements ListView.OnIte
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
-                loading = ProgressDialog.show(ViewAllEmployee.this,"Fetching Data","Wait...",false,false);
+                loading = ProgressDialog.show(ViewAllEmployee.this,"กำลังดึงข้อมูล","สักครู่...",false,false);
             }
 
             @Override
