3206c54bcde16a3eee24e75f9ecc3131473f88a1
spideryom <spideryom049@gmail.com>
2017-04-08 20:29:03 +0700

    เปลี่ยน Host : spider049.16mb.com

--- app/src/main/java/net/simplifiedcoding/mysqlcrud/MainActivity.java
+++ app/src/main/java/net/simplifiedcoding/mysqlcrud/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
